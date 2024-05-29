f77c66a77f52498733dd430264e9732db510ab78
Tom Evans <tomevans@toms-mbp.speedport.ip>
2024-05-29 16:19:18 +0200

    third table added

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,18 +5,20 @@ import '@radix-ui/themes/styles.css';
 
 import eventsData from '../public/events_table.json'
 import museumData from '../public/museum_recs.json'
+import filmData from '../public/film_recs.json'
 
 export default function Home() {
   return (
     <Flex direction="column" gap="9" style={{ maxWidth: '960px', margin: '5rem auto 0' }}>
       <Flex direction="column" gap="6" align="center">
-        <img src="vercel.svg" style={{ width: '150px' }} />
+        <img src="Berlin.svg.png" style={{ width: '50px' }} />
         <Heading size="9">Berlin Culture Vulture</Heading>
-        <img src="next.svg" style={{ width: '150px' }} />
+        <img src="ber-cult.jpeg" style={{ width: '1000px' }} />
       </Flex>
 
-      <Tabs.Root defaultValue='events'>
+      <Tabs.Root defaultValue='films' align="center">
         <Tabs.List>
+          <Tabs.Trigger value="films">Films</Tabs.Trigger>
           <Tabs.Trigger value="events">Events</Tabs.Trigger>
           <Tabs.Trigger value="museum">Museum</Tabs.Trigger>
         </Tabs.List>
@@ -70,6 +72,41 @@ export default function Home() {
                       <Table.Cell>
                         <Button variant="solid" asChild>
                           <Link href={data.h3} target='_blank'>Book now</Link>
+                        </Button>
+                      </Table.Cell>
+                    </Table.Row>
+                  )
+                })}
+              </Table.Body>
+            </Table.Root>
+
+          </Tabs.Content>
+
+          <Tabs.Content value="films">
+
+            <Table.Root>
+              <Table.Header>
+                <Table.Row>
+                  <Table.ColumnHeaderCell>Title</Table.ColumnHeaderCell>
+                  <Table.ColumnHeaderCell>IMDB Rating</Table.ColumnHeaderCell>
+                  <Table.ColumnHeaderCell></Table.ColumnHeaderCell>
+                  <Table.ColumnHeaderCell>Link</Table.ColumnHeaderCell>
+                  <Table.ColumnHeaderCell style={{ width: '113px' }} />
+                </Table.Row>
+              </Table.Header>
+
+              <Table.Body>
+                {filmData.map((data, idx) => {
+                  return (
+                    <Table.Row key={idx}>
+                      <Table.Cell>{data.Title}</Table.Cell>
+                      <Table.RowHeaderCell>{data['Aggregate Rating']}</Table.RowHeaderCell>
+                      <Table.Cell>
+                        <img src={data['Image URL']} alt={data.Title} style={{ width: '100px', height: 'auto' }} />
+                      </Table.Cell>
+                      <Table.Cell>
+                        <Button variant="solid" asChild>
+                          <Link href={data.Link} target='_blank'>Book now</Link>
                         </Button>
                       </Table.Cell>
                     </Table.Row>
